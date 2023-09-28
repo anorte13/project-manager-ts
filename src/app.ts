343756294fb80e5b16b6fed8e69325311541f8b3
Angel <angel-ortega27@hotmail.com>
2023-09-28 13:36:44 -0700

    added Class to handle project submission

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,54 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  root: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.root = document.getElementById("app") as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    /* Grabbing form HTML Elements */
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    /* Call helper functions to append elements and handle submit to the DOM */
+    this.configure();
+    this.attach();
+  }
+
+  private handleSubmit(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.handleSubmit.bind(this));
+  }
+
+  private attach() {
+    this.root.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const formInput = new ProjectInput();
